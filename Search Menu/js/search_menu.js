27e056870ecb64f2a8fc1a640805e8436030fc39
machineJK <77032693+machineJK@users.noreply.github.com>
2021-07-05 08:57:11 +0900

    Search Menu, Fixed Sidebar
    Search Menu, Fixed Sidebar

---
+++ Search Menu/js/search_menu.js
@@ -0,0 +1,30 @@
+// document.addEventListener("DOMContentLoaded",function(){
+//     let srch = document.getElementsByClassName("srch")[0];
+//     let tabs = document.getElementsByClassName("tabs");
+//     srch.addEventListener("keyup",function(){
+//         // tabs[0].style.display = "none";
+        
+//         let val = srch.value.toLowerCase();
+//         for(let i = 0; i < tabs.length; i++){
+//             let txt = tabs[i].textContent.toLowerCase();
+//             if(val == ""){
+//                 tabs[i].style.display = "block";
+//             }else if(txt.indexOf(val) != -1){
+//                 tabs[i].style.display = "block";
+//             }else if(txt.indexOf(val) == -1){
+//                 tabs[i].style.display = "none";
+//             }
+//         }
+//     });
+// });
+
+
+
+//////// jQuery ////////
+
+
+
+
+
+
+
